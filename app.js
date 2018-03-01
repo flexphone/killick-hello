0dc90d9a68f9c3b9b01447545bd1b1295c1eaed2
Thomas Lawson <Thomas@Daniels-MacBook-Pro-2.local>
2018-03-01 14:08:23 -0700

    killick-hello

--- app.js
+++ app.js
@@ -4,16 +4,31 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-var index = require("./routes/index");
-var users = require("./routes/users");
+var isProduction = process.env.NODE_ENV === "production";
 
 var app = express();
 
 // view engine setup
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+//connect to our database monogo
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect("mongodb://localhost/killick", function(err) {
+    if (err) return console.error(err);
+    console.log("THE DB, mongo, is connected, and I ROCK");
+  });
+  mongoose.set("debug", true);
+}
+
+//import models as soon as we are connected!!!!!!
+require("./models/Article");
+app.use(require("./routes"));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
@@ -25,17 +40,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/api", function(req, res, next) {
   res.send("API all good in the hood!");
 });
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client-app/build/index.html"));
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
 });
 
 // error handler
